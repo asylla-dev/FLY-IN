9556955cf7d9ecd9658b8989a076dd4a0cc35dca
asylla <asylla@student.42belgium.be>
2026-08-05 12:40:03 +0200

    *

---
+++ src/simulation.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+from dataclasses import dataclass
+from typing import Optional
+from .models import Connection, Zone, ZoneType, 
+
+
+@dataclass
+class Drone:
+    
